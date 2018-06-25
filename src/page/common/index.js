533b3a2423a0f6c5b7bb4463d5be820dd9764b42
hui-fly <1561464710@qq.com>
2018-06-25 19:29:36 +0800

    完成通用样式

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,7 +2,8 @@
 * @Author: hui-fly
 * @Date:   2018-06-14 18:33:22
 * @Last Modified by:   hui-fly
-* @Last Modified time: 2018-06-25 18:22:14
+* @Last Modified time: 2018-06-25 19:24:31
 */
 require('./layout.css');
-require('./footer/index.css')
+// require('node_modules/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,16 +2,7 @@
 * @Author: hui-fly
 * @Date:   2018-06-14 18:20:21
 * @Last Modified by:   hui-fly
-* @Last Modified time: 2018-06-25 18:39:16
+* @Last Modified time: 2018-06-25 18:43:10
 */
 require('./index.css');
 var _mm = require('util/mm.js');
-_mm.request({
-	url:'/product/list.do?keyword=1',
-    success:function(res){
-    	alert("chengg")
-    },
-    error:function(errMsg){
-    	alert("哈哈")
-    }
-})
